682d509fd3d6b02ab6e334a473a23973f5171ffe
AyazMansur <mansur.ayazzz@gmail.com>
2022-10-27 21:05:54 -0400

    modified from Ayaz PC_with nirali_1

--- Main.py
+++ Main.py
@@ -1,10 +1,24 @@
-print("This is Main file")
+Number_1 = input("Select a number: ")
+Number_2 = input("Select another number: ")
+print("The numbers you selected are: " + Number_1 + " and " + Number_2)
 
-print("edited from pycharm")
+shu_karvu = input("what you want to do: add, sub, mul or div: ")
 
-print("this should be printed")
+if shu_karvu == "+":
+    Number_3 = int(Number_1) + int(Number_2)
+    print("The sum is " + str(Number_3))
 
+if shu_karvu == "-":
+    Number_3 = int(Number_1) - int(Number_2)
+    print(" The difference is : " + str(Number_3))
 
- #restored
+if shu_karvu == "*":
+    Number_3 = int(Number_1) * int(Number_2)
+    print("The product is " + str(Number_3))
 
- print("This is a new branch")
+if shu_karvu == "/":
+    Number_3 = int(Number_1) / int(Number_2)
+    print(" The divident is : " + str(Number_3))
+
+shu_karwavu_pachu = input(" what next?")
+
